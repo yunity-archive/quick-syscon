f53343ce1e041ea0eeb0c9b911e94f44fceffc40
ouya <alexander@andlabs.eu>
2016-06-23 23:19:34 +0200

    merge

---
+++ client/login/register.js
@@ -0,0 +1,12 @@
+Template.register.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        Accounts.createUser({
+            email: email,
+            password: password
+        });
+        Router.go('/'); // open main view
+    }
+});
---
+++ client/subscriptions.js
@@ -0,0 +1,2 @@
+Meteor.subscribe("proposals");
+Meteor.subscribe("votes");
