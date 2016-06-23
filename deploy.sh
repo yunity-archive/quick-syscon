7fd7661fda5cd373a8679c2de8ab8e809cd611df
Tilmann Becker <tilmann.becker@gmail.com>
2016-06-23 18:28:55 +0200

    try to fix deployment

--- deploy.sh
+++ deploy.sh
@@ -15,4 +15,4 @@ echo "deploying branch [$BRANCH] to [$HOST]"
 
 scp deploy/remote.sh $USERNAME@$HOST:deploy.sh
 scp deploy/quick-syscon.tar.gz $USERNAME@$HOST:quick-syscon.tar.gz
-ssh $USERNAME@$HOST ./deploy.sh
+ssh $USERNAME@$HOST /bin/sh deploy.sh
