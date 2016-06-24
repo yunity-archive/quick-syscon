d2d7c3f41947628f7bf0a86cbe28d26f06ebf839
Tilmann Becker <tilmann.becker@gmail.com>
2016-06-24 11:08:55 +0200

    add profile and changed loginform

--- client/profile/profile.js
+++ client/profile/profile.js
@@ -1,1 +1,15 @@
-
+Template.profile.helpers({
+  userdata: {
+    name: 'Tilmann',
+    mail: 'tilmi@beh.de',
+    slack: '@amazing_tilmann'
+  },
+  mygroups: [
+    { name: 'yunity PR' },
+    { name: 'FSINT' },
+  ],
+  othergroups: [
+    { name: 'Translation' },
+    { name: 'Development '}
+  ]
+});
